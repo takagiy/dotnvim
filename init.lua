367eb2f97b8fdea53c8c5d1b2f2985846759bc19
Yuki Takagi <takagiy.4dev@gmail.com>
2025-02-14 01:52:56 +0900

    fix

--- init.lua
+++ init.lua
@@ -197,21 +197,27 @@ require("lazy").setup({
       init = function()
         vim.g.barbar_auto_setup = false
       end,
-      keys = {
-        { "qq", "<Cmd>BufferClose<CR>" },
-        { "<Tab>", "<Cmd>BufferNext<CR>" },
-        { "<S-Tab>", "<Cmd>BufferPrevious<CR>" },
-        { "1", "<Cmd>BufferGoto 1<CR>" },
-        { "2", "<Cmd>BufferGoto 2<CR>" },
-        { "3", "<Cmd>BufferGoto 3<CR>" },
-        { "4", "<Cmd>BufferGoto 4<CR>" },
-        { "5", "<Cmd>BufferGoto 5<CR>" },
-        { "6", "<Cmd>BufferGoto 6<CR>" },
-        { "7", "<Cmd>BufferGoto 7<CR>" },
-        { "8", "<Cmd>BufferGoto 8<CR>" },
-        { "9", "<Cmd>BufferGoto 9<CR>" },
-        { "0", "<Cmd>BufferLast<CR>" },
-      },
+      config = function(plug, opts)
+        require("barbar").setup(opts)
+        local keys = {
+          { "qq", "<Cmd>BufferClose<CR>" },
+          { "<Tab>", "<Cmd>BufferNext<CR>" },
+          { "<S-Tab>", "<Cmd>BufferPrevious<CR>" },
+          { "1", "<Cmd>BufferGoto 1<CR>" },
+          { "2", "<Cmd>BufferGoto 2<CR>" },
+          { "3", "<Cmd>BufferGoto 3<CR>" },
+          { "4", "<Cmd>BufferGoto 4<CR>" },
+          { "5", "<Cmd>BufferGoto 5<CR>" },
+          { "6", "<Cmd>BufferGoto 6<CR>" },
+          { "7", "<Cmd>BufferGoto 7<CR>" },
+          { "8", "<Cmd>BufferGoto 8<CR>" },
+          { "9", "<Cmd>BufferGoto 9<CR>" },
+          { "0", "<Cmd>BufferLast<CR>" },
+        }
+        for _, key in pairs(keys) do
+          vim.keymap.set("n", key[1], key[2], { noremap = true, silent = true })
+        end
+      end,
     },
     {
       "nvim-treesitter/nvim-treesitter",
